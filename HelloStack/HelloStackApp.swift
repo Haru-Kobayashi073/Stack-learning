01364a04bf96db7521bdd8b3501b8dc02ca52c73
Haru-Kobayashi073 <mndnethnbr.2108@gmail.com>
2022-11-08 12:12:19 +0900

    Initial Commit

---
+++ HelloStack/HelloStackApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloStackApp.swift
+//  HelloStack
+//
+//  Created by 小林春 on 2022/11/08.
+//
+
+import SwiftUI
+
+@main
+struct HelloStackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
